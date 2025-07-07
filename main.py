252b9f6bfdc838f1f2a2ded6ff2453cca7a2e441
Cristiano Pereira <cristianopereiraramos9@gmail.com>
2025-07-07 20:56:25 -0300

    add initial project files and configurations, including .gitignore, Python script, and XML settings

---
+++ main.py
@@ -0,0 +1,6 @@
+def saudacao(nome):
+    return f"Olá, {nome}! Bem-vindo ao meu projeto Python."
+
+if __name__ == "__main__":
+    print(saudacao("Mundo"))
+    print("Este é o seu novo projeto Python!")
